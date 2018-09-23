0e46b17388f7af5a57eedc91dfc0ced8aecee5f3
Cara Morelli <caramorelli7@gmail.com>
2018-09-23 05:34:29 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown(count)
   # count.downto(0) { |num| puts num.to_s + ' SECOND(S)!' }
   # 'HAPPY NEW YEAR!'
   while count >= 0
-    puts count.to_i + " SECOND(S)!" 
+    puts count.to_s + " SECOND(S)!" 
     count -= 1
   end 
   puts 'HAPPY NEW YEAR!'

ac5b2c90554f05c946fc4256f30a90cbf31b6599
Cara Morelli <caramorelli7@gmail.com>
2018-09-23 05:58:52 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,11 @@
 #write your code here
 
 def countdown(count)
-  # count.downto(0) { |num| puts num.to_s + ' SECOND(S)!' }
-  # puts 'HAPPY NEW YEAR!'
   return 'HAPPY NEW YEAR!' if count == 12
   while count > 0
     puts "#{count} SECOND(S)!" 
     count -= 1
   end 
-  # puts 'HAPPY NEW YEAR!'
 end
 
 def countdown_with_sleep(num)

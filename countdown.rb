9e61efe8eac6042a0ddfc36397527bcd5d0d00d6
Cara Morelli <caramorelli7@gmail.com>
2018-09-23 05:46:36 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,7 @@ def countdown(count)
   # puts 'HAPPY NEW YEAR!'
   return 'HAPPY NEW YEAR!' if count == 12
   int = 0
-  while int >= 0
+  while int <= count
     puts count.to_s + " SECOND(S)!" 
     count -= 1
   end 
